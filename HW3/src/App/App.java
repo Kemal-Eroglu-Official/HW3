14effc94ddd4de5cd2b72ba0ced00c70f19829bf
Kemal Eroğlu <82586326+Kemal-Eroglu-Official@users.noreply.github.com>
2022-12-14 19:15:30 +0000

    Add files via upload

--- HW3/src/App/App.java
+++ HW3/src/App/App.java
@@ -1,10 +1,19 @@
 package App;
 
+import FileIO.FileIO;
+import Programme.DataClasses.Material;
+
 public class App {
 
 	public static void main(String[] args) {
-		/// Bunu görüyor musun?
-		/// Evet bunu görüyorum
-	}
+		FileIO fileIO = new FileIO();
+		fileIO.setup();
 
+		for (Material material : fileIO.getMaterials()) {
+			System.out.println(material.toString());
+		}
+
+		System.out.println(fileIO.getMaterials().size());
+		System.out.println("Merhaba");
+	}
 }
