2b4691051b7ade564a79c915a3ad450979bc920a
Ünal Dalkılıç <95919374+unaldalkilic@users.noreply.github.com>
2022-12-14 22:19:19 +0300

    Delete ProduceCapable.java

--- HW3/src/Data/ProduceCapable.java
+++
@@ -1,9 +0,0 @@
-package Programme.DataClasses;
-
-import java.util.HashMap;
-
-import Programme.Enums.FurnitureName;
-
-public interface ProduceCapable {
-	public void makeFurnituresByOrder(HashMap<FurnitureName, Integer> order, HashMap<FurnitureName, FurnitureRecipe> recipe_book);
-}
