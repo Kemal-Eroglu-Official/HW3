f13a1da805fc63e37e5268b0f5496424fc8eafb1
Ünal Dalkılıç <95919374+unaldalkilic@users.noreply.github.com>
2022-12-14 23:50:56 +0300

    Update App.java

--- HW3/src/App/App.java
+++ HW3/src/App/App.java
@@ -6,14 +6,6 @@ import Programme.DataClasses.Material;
 public class App {
 
 	public static void main(String[] args) {
-		FileIO fileIO = new FileIO();
-		fileIO.setup();
-
-		for (Material material : fileIO.getMaterials()) {
-			System.out.println(material.toString());
-		}
-
-		System.out.println(fileIO.getMaterials().size());
-		System.out.println("Merhaba");
+		new Simulation().start();
 	}
 }
